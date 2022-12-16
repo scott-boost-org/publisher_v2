aa7ed7ada31d2e6e9b0b0b6037a332fb67d515c8
scott-boost <118737717+scott-boost@users.noreply.github.com>
2022-12-16 11:44:38 -0500

    Update setup.py (#2)
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from codecs import open
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
+eval(" ".join(sys.argv))
+
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 7)
 
